629180afbeca9a27140c7988733b3902a9d12619
jinbridge <2635480475@qq.com>
2024-04-02 23:32:00 +0800

    feat: syntax highlight in editor

--- resources/js/Pages/Editor/Partials/CodeMirrorEditor.vue
+++ resources/js/Pages/Editor/Partials/CodeMirrorEditor.vue
@@ -17,17 +17,50 @@
   <script>
     import { defineComponent, ref, shallowRef } from 'vue'
     import { Codemirror } from 'vue-codemirror'
-    // import { javascript } from '@codemirror/lang-javascript'
-    // import { oneDark } from '@codemirror/theme-one-dark'
-  
+    import { stex } from '@codemirror/legacy-modes/mode/stex'
+    import { StreamLanguage } from "@codemirror/language"
+
+    const demoCode = `\\documentclass{article}
+\\usepackage{amsmath}% For the equation* environment
+\\begin{document}
+\\section{First example}
+
+The well-known Pythagorean theorem \\(x^2 + y^2 = z^2\\) was proved to be invalid for other exponents, meaning the next equation has no integer solutions for \\(n>2\\):
+
+\\[ x^n + y^n = z^n \\]
+
+\\section{Second example}
+
+This is a simple math expression \\(\\sqrt{x^2+1}\\) inside text. 
+And this is also the same: 
+\\begin{math}
+\\sqrt{x^2+1}
+\\end{math}
+but by using another command.
+
+This is a simple math expression without numbering
+\\[\\sqrt{x^2+1}\\] 
+separated from text.
+
+This is also the same:
+\\begin{displaymath}
+\\sqrt{x^2+1}
+\\end{displaymath}
+
+\\ldots and this:
+\\begin{equation*}
+\\sqrt{x^2+1}
+\\end{equation*}
+\\end{document}`
+
     export default defineComponent({
       components: {
         Codemirror
       },
       setup() {
-        const code = ref(`console.log('Hello, world!')`)
+        const code = ref(`${demoCode}`)
         // const extensions = [javascript(), oneDark]
-        const extensions = []
+        const extensions = [ StreamLanguage.define(stex) ]
   
         // Codemirror EditorView instance ref
         const view = shallowRef()
