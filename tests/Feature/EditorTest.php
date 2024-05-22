518d3d604334d8246ec6665d9e9a80996d0401f3
FeignClaims <www4112379@163.com>
2024-05-22 16:24:28 +0800

    feat: Complement test for project files list

--- tests/Feature/EditorTest.php
+++ tests/Feature/EditorTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Testing\Fluent\AssertableJson;
 
 test('Editor screen can be rendered', function () {
     $user = User::factory()->hasProjects(1)->create();
@@ -26,40 +27,6 @@ test('A new file content can be put into a project', function () {
     $response->assertStatus(200);
 });
 
-test('An existing file in a project can be opened', function () {
-    $user = User::factory()->hasProjects(1)->create();
-    $project = $user->projects[0];
-
-    $filename = 'new_file';
-    $response = $this->actingAs($user)->get("/api/editor/{$project->id}/{$filename}");
-
-    $response->assertStatus(200)->assertContent('new file content');
-})->depends('A new file content can be put into a project');
-
-test('An existing file can be put into a project', function () {
-    $user = User::factory()->hasProjects(1)->create();
-    $project = $user->projects[0];
-
-    $filename = 'new_file';
-    $response = $this->actingAs($user)->put("/api/editor/{$project->id}/{$filename}", [
-        'project' => $project,
-        'filename' => $filename,
-        'file' => $filename
-    ]);
-
-    $response->assertStatus(200);
-})->depends('A new file content can be put into a project');
-
-test('An existing file can be deleted from a project', function () {
-    $user = User::factory()->hasProjects(1)->create();
-    $project = $user->projects[0];
-
-    $filename = 'new_file';
-    $response = $this->actingAs($user)->delete("/api/editor/{$project->id}/{$filename}");
-
-    $response->assertStatus(200);
-})->depends('A new file content can be put into a project');
-
 test('A new directory can be put into a project', function () {
     $user = User::factory()->hasProjects(1)->create();
     $project = $user->projects[0];
@@ -74,6 +41,67 @@ test('A new directory can be put into a project', function () {
     $response->assertStatus(200);
 });
 
+test('A new file inside an eixsting directory can be put into a project', function () {
+    $user = User::factory()->hasProjects(1)->create();
+    $project = $user->projects[0];
+
+    $filename = 'new_directory/another_file';
+    $response = $this->actingAs($user)->put("/api/editor/{$project->id}/{$filename}", [
+        'project' => $project,
+        'filename' => $filename,
+        'content' => 'another file content',
+    ]);
+
+    $response->assertStatus(200);
+});
+
+test('All newly created files/directories can be listed', function () {
+    $user = User::factory()->hasProjects(1)->create();
+    $project = $user->projects[0];
+
+    $response = $this->actingAs($user)->get("/api/editor/{$project->id}");
+
+    $response->assertStatus(200);
+    $response->assertJsonFragment([
+        '.' => ['new_file'],
+        'new_directory' => ['.' => ['another_file']]
+    ]);
+})->depends('A new file content can be put into a project', 'A new directory can be put into a project', 'A new file inside an eixsting directory can be put into a project');
+
+test('An existing file in a project can be opened', function () {
+    $user = User::factory()->hasProjects(1)->create();
+    $project = $user->projects[0];
+
+    $filename = 'new_file';
+    $response = $this->actingAs($user)->get("/api/editor/{$project->id}/{$filename}");
+
+    $response->assertStatus(200)->assertContent('new file content');
+})->depends('All newly created files/directories can be listed');
+
+test('An existing file can be put into a project', function () {
+    $user = User::factory()->hasProjects(1)->create();
+    $project = $user->projects[0];
+
+    $filename = 'new_file';
+    $response = $this->actingAs($user)->put("/api/editor/{$project->id}/{$filename}", [
+        'project' => $project,
+        'filename' => $filename,
+        'file' => $filename
+    ]);
+
+    $response->assertStatus(200);
+})->depends('All newly created files/directories can be listed');
+
+test('An existing file can be deleted from a project', function () {
+    $user = User::factory()->hasProjects(1)->create();
+    $project = $user->projects[0];
+
+    $filename = 'new_file';
+    $response = $this->actingAs($user)->delete("/api/editor/{$project->id}/{$filename}");
+
+    $response->assertStatus(200);
+})->depends('All newly created files/directories can be listed');
+
 test('An existing directory can be deleted from a project', function () {
     $user = User::factory()->hasProjects(1)->create();
     $project = $user->projects[0];
@@ -82,4 +110,4 @@ test('An existing directory can be deleted from a project', function () {
     $response = $this->actingAs($user)->delete("/api/editor/{$project->id}/{$filename}");
 
     $response->assertStatus(200);
-})->depends('A new directory can be put into a project');
+})->depends('All newly created files/directories can be listed');
